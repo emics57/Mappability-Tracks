e1df4208590eeef512f177ebbafd8ebbab63a32a
Emily Xu <emily.xu59@gmail.com>
2024-03-12 16:55:11 -0700

    new .py file using bedtools merge

--- mappabilityBEDtools.py
+++ mappabilityBEDtools.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import argparse
 import pysam
+import subprocess
 
 """
 mappability.py takes a BAM file as input and outputs the mappable regions as a BED file.
@@ -20,13 +21,15 @@ alignments = pysam.AlignmentFile(bamInput,"rb")
 pysam.set_verbosity(save)
 for read in alignments:
     readName = read.query_name # read id
-    derivedName = readName.split(':')[0] # chromosome the read was derived from
+    derivedChr = readName.split(':')[0] # chromosome the read was derived from
     flag = read.flag # SAM flag
     cigarSize = read.infer_query_length() # read size inferred from cigar string
-    rname = alignments.get_reference_name(read.reference_id) # mapped chromosome name
-    ref_start = read.reference_start # mapped coordinate
-    bam_data.append([readName,derivedName,flag,rname,cigarSize,ref_start])
+    mappedChr = alignments.get_reference_name(read.reference_id) # mapped chromosome name
+    readStart = read.reference_start # mapped coordinate
+    readEnd = read.reference_end
+    bam_data.append([readName,derivedChr,flag,mappedChr,cigarSize,readStart,readEnd])
 dataframe = pd.DataFrame(bam_data)
+size = dataframe.loc[0,4]
 chr = dataframe.loc[0,1]
 
 def createCoords(df):
@@ -41,32 +44,10 @@ def createCoords(df):
     unique_mapped_df[5] = unique_mapped_df[5].astype(int)
 
     # mapped coordinates of uniquely mapped reads
-    mappedCoord = list(zip(unique_mapped_df[5], unique_mapped_df[4]))
+    mappedCoord = list(zip(unique_mapped_df[5], unique_mapped_df[6]))
     mappedCoord.sort()
     mappedCoord = sorted(mappedCoord, key=lambda x: x[1])
-
-    # define uniquely mapped regions
-    tupleList=[]
-    start = mappedCoord[0][0]
-    size = mappedCoord[0][1]
-    end = start+size
-    for i in range(len(mappedCoord) - 1):
-        # if end coord less than the next largest start coord -> start new region 
-        size = mappedCoord[i][1]
-        currCoord = mappedCoord[i][0]
-        nextCoord = mappedCoord[i+1][0]
-        if currCoord+size < nextCoord:
-            # set mappable end coordinate to current mapped coordinate + size
-            end = currCoord + size
-            tupleList.append((start,end))
-            # set start coordinate to next largest start coordinate
-            start = nextCoord
-            # set end coordinate to start + size
-            end = start+size
-    end = mappedCoord[i+1][0]
-    tupleList.append((start,end))
-    print(tupleList)
-    return tupleList
+    return mappedCoord
 
 def tuples_to_bed(tuples_list, bed_file_path, chrom):
     """
@@ -80,11 +61,15 @@ def tuples_to_bed(tuples_list, bed_file_path, chrom):
 
 
 def main():
-    output_bed_file=parser.parse_args().out
+    merged_bed_file=parser.parse_args().out
     # define mappable regions
     coords = createCoords(dataframe)
-    # generate bed file
-    tuples_to_bed(coords, output_bed_file, chr)
+    # generate read Coordinate BED file
+    readCoordinatesBED = f"{size}bp.{chr}.readsCoordinates.bed"
+    tuples_to_bed(coords, readCoordinatesBED, chr)
+    # generate merged BED file
+    command = f"bedtools merge -i {readCoordinatesBED} -c 1 -o count > {merged_bed_file}"
+    subprocess.run(command, shell=True)
 
 if __name__ == "__main__":
     main()
